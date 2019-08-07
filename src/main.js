7d746d537e94f2458340d8f4bf3dc58613b1079c
冰风 <wuxiaoming@baosight.com>
2019-08-07 14:57:12 +0800

    完成最基础的前端搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = 'http://localhost:3000';
+Vue.prototype.$http = axios;
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
